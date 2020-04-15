759e32a32812622b19ae31be7f9a519ace08480c
SuryaSankar <suryashankar.m@gmail.com>
2020-04-16 00:30:53 +0530

    adding ipython to requirements.txt. Simplifying cli.py

--- app/views/main_pages_bp.py
+++ app/views/main_pages_bp.py
@@ -6,3 +6,8 @@ main_pages_bp = Blueprint('main_pages_bp', __name__)
 @main_pages_bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@main_pages_bp.route("/about")
+def about():
+    return render_template("about.html")
--- cli.py
+++ cli.py
@@ -1,27 +1,16 @@
-from contextlib import contextmanager
-import json
-from pprint import PrettyPrinter
-from toolspy import merge
-import app.models as models
 from app.app_factory import create_app
+import app.models as models
+import IPython
 
 
 def run_interactive_shell():
     app = create_app()
-    app.config['WTF_CSRF_ENABLED'] = False
 
     # Needed for making the console work in app request context
     ctx = app.test_request_context()
     ctx.push()
 
-    pprint = PrettyPrinter(indent=4).pprint
-
-    try:
-        import IPython
-        IPython.embed()
-    except:
-        import code
-        code.interact(local=merge(locals(), globals()))
+    IPython.embed()
 
 
 if __name__ == '__main__':
